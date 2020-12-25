b8b866f18523e06620ba829c3daae22e4d337ca3
Zachary Pitcher <zackpi@mit.edu>
2020-12-24 22:09:03 -0500

    made it build yippideedoodahhhhhh

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate piston_window;
 
-use piston_window::*;
-use piston::*;
+use piston_window::{PistonWindow, WindowSettings};
+use piston_window::{Event, Input, Button, ButtonState, Key, Loop, UpdateArgs, RenderArgs};
+use piston_window::{clear, rectangle};
 
 struct Vector {
 	x: f64,
@@ -29,81 +30,93 @@ struct Game {
 }
 
 fn main() {
-    let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
-        .exit_on_esc(true).build().unwrap();
+  let mut window: PistonWindow =
+    WindowSettings::new("Jump Game", [640, 480])
+    .exit_on_esc(true).build().unwrap();
 
-    let mut p1 = Player {
-    	pos: Vector{x:0.0, y:0.0},
-    	vel: Vector{x:0.0, y:0.0},
-    	acc: Vector{x:0.0, y:0.0},
-    	size: Vector{x:100.0, y:100.0},
-    	color: [1.0, 0.0, 0.0, 1.0],
-    	keys: KeyState{left: false, right: false, jump: false}
-    };
+  let mut p1 = Player {
+  	pos: Vector{x:0.0, y:0.0},
+  	vel: Vector{x:0.0, y:0.0},
+  	acc: Vector{x:0.0, y:0.0},
+  	size: Vector{x:100.0, y:100.0},
+  	color: [1.0, 0.0, 0.0, 1.0],
+  	keys: KeyState{left: false, right: false, jump: false}
+  };
 
-    let mut p2 = Player {
-    	pos: Vector{x:200.0, y:0.0},
-    	vel: Vector{x:0.0, y:0.0},
-    	acc: Vector{x:0.0, y:0.0},
-    	size: Vector{x:100.0, y:100.0},
-    	color: [1.0, 0.0, 0.0, 1.0],
-    	keys: KeyState{left: false, right: false, jump: false}
-    };
-    
-    let mut game = Game {
-    	p1: p1,
-    	p2: p2
-    };
+  let mut p2 = Player {
+  	pos: Vector{x:200.0, y:0.0},
+  	vel: Vector{x:0.0, y:0.0},
+  	acc: Vector{x:0.0, y:0.0},
+  	size: Vector{x:100.0, y:100.0},
+  	color: [1.0, 0.0, 0.0, 1.0],
+  	keys: KeyState{left: false, right: false, jump: false}
+  };
+  
+  let mut game = Game {
+  	p1: p1,
+  	p2: p2
+  };
 
-    while let Some(event) = window.next() {
-	    match event {
-	        Event::Input(input) => { process_keys(&game, &input); },
-	        Event::Update(update_args) => { update(&game); }
-	        Event::Render(render_args) => {
-	        	window.draw_2d(&event, |context, graphics, _device| {
-			        clear([1.0; 4], graphics);
-			        rectangle(game.p1.color, // red
-			                  [game.p1.pos.x, game.p1.pos.y, game.p1.size.x, game.p1.size.y],
-			                  context.transform,
-			                  graphics);
-			    });
-	        }
-	    }
-        
+  while let Some(event) = window.next() {
+    match event {
+      Event::Input(input_args, _timestamp) => { process_keys(&mut game, &input_args); },
+      Event::Loop(loop_args) => { 
+      	match loop_args {
+      		Loop::Update(update_args) => {
+      			update(&mut game, &update_args); 
+      		},
+      		Loop::Render(render_args) => { 
+      			render(&game, &mut window, event, &render_args); 
+      		},
+      		_ => {}
+      	}},
+      _ => {}
     }
+  }        
 }
 
-fn process_keys(game: &Game, input: &Input) {
+fn process_keys(game: &mut Game, input: &Input) {
 	match input {
-        Input::Press(butpress) => { 
-        	match butpress {
-	            Button::Keyboard(Key::Space) => { game.p1.keys.jump = true; }
-	            Button::Keyboard(Key::Left) => { game.p1.keys.left = true; }
-	            Button::Keyboard(Key::Right) => { game.p1.keys.right = true; }
-	            _ => {}
-        }}
-        Input::Release(butrelease) => {
-        	match butrelease {
-	            Button::Keyboard(Key::Space) => { game.p1.keys.jump = false; }
-	            Button::Keyboard(Key::Left) => { game.p1.keys.left = false; }
-	            Button::Keyboard(Key::Right) => { game.p1.keys.right = false; }
-	            _ => {}
-        }}
-    }
+    Input::Button(butargs) => {
+    	match butargs.button {
+        Button::Keyboard(Key::Space) => { 
+        	if !game.p1.keys.jump && butargs.state == ButtonState::Press { 
+        		game.p1.keys.jump = true;
+        	}
+        	if game.p1.keys.jump && butargs.state == ButtonState::Release {
+        		game.p1.keys.jump = false;
+        	} 
+        },
+        Button::Keyboard(Key::Left) => { 
+        	if !game.p1.keys.left && butargs.state == ButtonState::Press { 
+        		game.p1.keys.left = true; 
+        	}
+        	if game.p1.keys.left && butargs.state == ButtonState::Release {
+        		game.p1.keys.left = false;
+        	}
+       	},
+        Button::Keyboard(Key::Right) => {
+        	if !game.p1.keys.right && butargs.state == ButtonState::Press { 
+        		game.p1.keys.right = true; 
+        	}
+        	if game.p1.keys.right && butargs.state == ButtonState::Release {
+        		game.p1.keys.right = false;
+        	}
+        },
+        _ => {}
+    }},
+    _ => {}
+  }
 }
 
-fn update(game: &Game) {
+fn update(game: &mut Game, update_args: &UpdateArgs) {
 	game.p1.pos.x += 5.0;
 	game.p2.pos.x -= 5.0;
 }
 
-// fn render(game: &Game, window: &PistonWindow, event: &dyn GenericEvent) {
-//     window.draw_2d(&event, |context, graphics, _device| {
-//         clear([1.0; 4], graphics);
-//         rectangle(game.p1.color, // red
-//                   [game.p1.pos.x, game.p1.pos.y, game.p1.size.x, game.p1.size.y],
-//                   context.transform,
-//                   graphics);
-//     });
-// }
+fn render(game: &Game, window: &mut PistonWindow, event: Event, render_args: &RenderArgs){
+	window.draw_2d(&event, |c, g, _d| {
+    clear([1.0; 4], g);	 // clear the screen 
+    rectangle([0.0, 0.0, 0.0, 1.0], [0.0, 0.0, 100.0, 100.0], c.transform, g);	// draw the background
+  });
+}
