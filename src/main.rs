f88565600087e6a426783f9d6260aabe55012cf2
Catherine Zeng <yczeng@mit.edu>
2020-12-27 18:32:54 -0500

    structured into proper server v. client division

--- src/main.rs
+++ src/main.rs
@@ -59,22 +59,10 @@ fn main() {
     drift_time: 0,
     grounded: false
   };
-
-  let p2 = Player {
-  	pos: Vector{x:globals::WIDTH - 2.0*globals::BOX_WIDTH, y:2.0*globals::BOX_HEIGHT+0.1},
-  	vel: Vector{x:0.0, y:0.0},
-  	acc: Vector{x:0.0, y:0.0},
-  	size: Vector{x:2.0*globals::BOX_WIDTH, y:2.0*globals::BOX_HEIGHT},
-  	color: [0.0, 1.0, 0.0, 1.0],
-  	keys: KeyState{left: false, right: false, jump: false},
-    jump_time: 0,
-    drift_time: 0,
-    grounded: false
-  };
   
   let mut game = Game {
   	p1: p1,
-  	p2: p2
+  	// p2: p2
   };
 
   // connect to web socket
@@ -87,143 +75,23 @@ fn main() {
     .unwrap();
 
   println!("Successfully connected");
-  
-  // TODO:
-  // receive game state from server, populate game struct
-  // create a new player
-  // send the new game state to the server
 
   while let Some(event) = window.next() {
     match event {
-      Event::Input(input_args, _timestamp) => { process_keys(&mut game, &input_args); },
+      Event::Input(input_args, _timestamp) => { send_key(&mut game, &input_args); },
       Event::Loop(loop_args) => {
-      	match loop_args {
-      		Loop::Update(update_args) => { update(&mut game, &update_args); },
-      		Loop::Render(render_args) => { render(&game, &mut window, event, &render_args); },
-      		_ => {}
-      	}},
+        match loop_args {
+          // Loop::Update(update_args) => { update(&mut game, &update_args); },
+          Loop::Render(render_args) => { render(&game, &mut window, event, &render_args); },
+          _ => {}
+        }},
       _ => {}
     }
   }
 }
 
-fn process_keys(game: &mut Game, input: &Input) {
-	match input {
-    Input::Button(butargs) => {
-    	match butargs.button {
-        Button::Keyboard(Key::Space) => { 
-        	if !game.p1.keys.jump && butargs.state == ButtonState::Press { 
-        		game.p1.keys.jump = true;
-        	}
-        	if game.p1.keys.jump && butargs.state == ButtonState::Release {
-        		game.p1.keys.jump = false;
-        	} 
-        },
-        Button::Keyboard(Key::Left) => { 
-        	if !game.p1.keys.left && butargs.state == ButtonState::Press { 
-        		game.p1.keys.left = true; 
-        	}
-        	if game.p1.keys.left && butargs.state == ButtonState::Release {
-        		game.p1.keys.left = false;
-        	}
-       	},
-        Button::Keyboard(Key::Right) => {
-        	if !game.p1.keys.right && butargs.state == ButtonState::Press { 
-        		game.p1.keys.right = true; 
-        	}
-        	if game.p1.keys.right && butargs.state == ButtonState::Release {
-        		game.p1.keys.right = false;
-        	}
-        },
-        _ => {}
-    }},
-    _ => {}
-  }
-}
-
-fn update(game: &mut Game, update_args: &UpdateArgs) {
-  update_player(&mut game.p1, update_args.dt);
-  // update_player(&mut game.p2, update_args.dt);
-}
-
-fn update_player(p: &mut Player, dt: f64) {
-	// determine accelerations based on states
-	p.acc.y = -globals::GRAVITY;
-	if p.keys.jump {
-    if p.jump_time < globals::JUMP_DURATION {
-      p.acc.y = globals::JUMP_FORCE;
-      p.jump_time += 1;
-    }
-  } else if p.grounded {
-    p.jump_time = 0;
-  } else {
-    p.jump_time = globals::JUMP_DURATION;
-  }
-
-	if p.keys.left { 
-    if p.drift_time < globals::DRIFT_DURATION {
-      p.acc.x = -globals::DRIFT_FORCE;
-      p.drift_time += 1;
-    }
-  } else if p.keys.right {
-    if p.drift_time < globals::DRIFT_DURATION {
-      p.acc.x = globals::DRIFT_FORCE;
-      p.drift_time += 1;
-    }
-  } else {
-    if p.vel.x >= globals::MIN_XSPEED {
-      if p.grounded {
-        p.acc.x -= globals::GND_DRAG_FORCE;
-      } else {
-        p.acc.x -= globals::AIR_DRAG_FORCE;
-      }
-      if p.vel.x + p.acc.x < globals::MIN_XSPEED {
-        p.vel.x = 0.0;
-        p.acc.x = 0.0;
-      }
-    }
-    if p.vel.x <= -globals::MIN_XSPEED {
-      if p.grounded {
-        p.acc.x += globals::GND_DRAG_FORCE;
-      } else {
-        p.acc.x += globals::AIR_DRAG_FORCE;
-      }
-      if p.vel.x + p.acc.x > -globals::MIN_XSPEED {
-        p.vel.x = 0.0;
-        p.acc.x = 0.0;
-      }
-    }
-    p.drift_time = 0;
-  }
-
-	// integrate acceleration to get velocities
-	p.vel.x += p.acc.x * dt;
-	p.vel.y += p.acc.y * dt;
-
-	// integrate velocities to get positions
-	p.pos.x += p.vel.x * dt;
-	p.pos.y += p.vel.y * dt;
-
-  // do bounds checks (top and left)
-  if p.pos.y <= globals::BOX_HEIGHT {  // floor
-    p.pos.y = globals::BOX_HEIGHT + globals::MIN_YSPEED*dt;
-    p.vel.y = 0.0;
-  } else if p.pos.y >= globals::HEIGHT {  // ceiling
-    p.pos.y = globals::HEIGHT - globals::MIN_YSPEED*dt;
-    p.vel.y = 0.0;
-  }
-  if p.pos.x <= 0.0 {  // left
-    p.pos.x = 0.0 + globals::MIN_XSPEED*dt;
-    p.vel.x = 0.0;
-  } else if p.pos.x >= globals::WIDTH {  // right
-    p.pos.x = globals::WIDTH - globals::MIN_XSPEED*dt;
-    p.vel.x = 0.0;
-  }
-
-  p.grounded = p.pos.y <= globals::BOX_HEIGHT + globals::MIN_YSPEED*dt;
-  // println!("P[x:{}, y:{}, vx: {}, vy:{}, ax:{}, ay:{}]", p.pos.x, p.pos.y, p.vel.x, p.vel.y, p.acc.x, p.acc.y);
-
-  // TODO: for all of these updates, send a message to server saying that the player's info has changed.
+fn send_key(game: &mut Game, input: &Input) {
+  // send the keys to websocket
 }
 
 fn render(game: &Game, window: &mut PistonWindow, event: Event, render_args: &RenderArgs){
@@ -232,9 +100,8 @@ fn render(game: &Game, window: &mut PistonWindow, event: Event, render_args: &Re
 
 	window.draw_2d(&event, |c, g, _d| {
     clear([0.0, 0.0, 0.0, 1.0], g);	 // clear the screen
-
     rectangle(p1.color,	to_window_coords(&p1), c.transform, g);  // draw player 1
-    rectangle(p2.color, to_window_coords(&p2), c.transform, g);  // draw player 2
+    // rectangle(p2.color, to_window_coords(&p2), c.transform, g);  // draw player 2
   });
 }
 
--- src/server.rs
+++ src/server.rs
@@ -60,7 +60,7 @@ fn main() {
 
 			for message in receiver.incoming_messages() {
 				// TODO: whenever a message is received, it should contain the new state of the client that sent the action
-				// need to update state and broadcast new global state to all connected clients
+				// need to update game state and broadcast new global state to all connected clients
 
 				let message = message.unwrap();
 
@@ -80,4 +80,124 @@ fn main() {
 			}
 		});
 	}
+}
+
+fn process_keys(game: &mut Game, input: &Input) {
+	match input {
+    Input::Button(butargs) => {
+    	match butargs.button {
+        Button::Keyboard(Key::Space) => { 
+        	if !game.p1.keys.jump && butargs.state == ButtonState::Press { 
+        		game.p1.keys.jump = true;
+        	}
+        	if game.p1.keys.jump && butargs.state == ButtonState::Release {
+        		game.p1.keys.jump = false;
+        	} 
+        },
+        Button::Keyboard(Key::Left) => { 
+        	if !game.p1.keys.left && butargs.state == ButtonState::Press { 
+        		game.p1.keys.left = true; 
+        	}
+        	if game.p1.keys.left && butargs.state == ButtonState::Release {
+        		game.p1.keys.left = false;
+        	}
+       	},
+        Button::Keyboard(Key::Right) => {
+        	if !game.p1.keys.right && butargs.state == ButtonState::Press { 
+        		game.p1.keys.right = true; 
+        	}
+        	if game.p1.keys.right && butargs.state == ButtonState::Release {
+        		game.p1.keys.right = false;
+        	}
+        },
+        _ => {}
+    }},
+    _ => {}
+  }
+}
+
+fn update(game: &mut Game, update_args: &UpdateArgs) {
+  // rewrite for each player in game, update player
+  update_player(&mut game.p1, update_args.dt);
+  // update_player(&mut game.p2, update_args.dt);
+}
+
+fn update_player(p: &mut Player, dt: f64) {
+	// determine accelerations based on states
+	p.acc.y = -globals::GRAVITY;
+	if p.keys.jump {
+    if p.jump_time < globals::JUMP_DURATION {
+      p.acc.y = globals::JUMP_FORCE;
+      p.jump_time += 1;
+    }
+  } else if p.grounded {
+    p.jump_time = 0;
+  } else {
+    p.jump_time = globals::JUMP_DURATION;
+  }
+
+	if p.keys.left { 
+    if p.drift_time < globals::DRIFT_DURATION {
+      p.acc.x = -globals::DRIFT_FORCE;
+      p.drift_time += 1;
+    }
+  } else if p.keys.right {
+    if p.drift_time < globals::DRIFT_DURATION {
+      p.acc.x = globals::DRIFT_FORCE;
+      p.drift_time += 1;
+    }
+  } else {
+    if p.vel.x >= globals::MIN_XSPEED {
+      if p.grounded {
+        p.acc.x -= globals::GND_DRAG_FORCE;
+      } else {
+        p.acc.x -= globals::AIR_DRAG_FORCE;
+      }
+      if p.vel.x + p.acc.x < globals::MIN_XSPEED {
+        p.vel.x = 0.0;
+        p.acc.x = 0.0;
+      }
+    }
+    if p.vel.x <= -globals::MIN_XSPEED {
+      if p.grounded {
+        p.acc.x += globals::GND_DRAG_FORCE;
+      } else {
+        p.acc.x += globals::AIR_DRAG_FORCE;
+      }
+      if p.vel.x + p.acc.x > -globals::MIN_XSPEED {
+        p.vel.x = 0.0;
+        p.acc.x = 0.0;
+      }
+    }
+    p.drift_time = 0;
+  }
+
+	// integrate acceleration to get velocities
+	p.vel.x += p.acc.x * dt;
+	p.vel.y += p.acc.y * dt;
+
+	// integrate velocities to get positions
+	p.pos.x += p.vel.x * dt;
+	p.pos.y += p.vel.y * dt;
+
+  // do bounds checks (top and left)
+  if p.pos.y <= globals::BOX_HEIGHT {  // floor
+    p.pos.y = globals::BOX_HEIGHT + globals::MIN_YSPEED*dt;
+    p.vel.y = 0.0;
+  } else if p.pos.y >= globals::HEIGHT {  // ceiling
+    p.pos.y = globals::HEIGHT - globals::MIN_YSPEED*dt;
+    p.vel.y = 0.0;
+  }
+  if p.pos.x <= 0.0 {  // left
+    p.pos.x = 0.0 + globals::MIN_XSPEED*dt;
+    p.vel.x = 0.0;
+  } else if p.pos.x >= globals::WIDTH {  // right
+    p.pos.x = globals::WIDTH - globals::MIN_XSPEED*dt;
+    p.vel.x = 0.0;
+  }
+
+  p.grounded = p.pos.y <= globals::BOX_HEIGHT + globals::MIN_YSPEED*dt;
+  // println!("P[x:{}, y:{}, vx: {}, vy:{}, ax:{}, ay:{}]", p.pos.x, p.pos.y, p.vel.x, p.vel.y, p.acc.x, p.acc.y);
+
+  // TODO: for all of these updates, send a message to server saying that the player's info has changed.
 }
