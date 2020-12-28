a5cd08f48d16a6b4867e8f082098d1cb6a09fd45
Catherine Zeng <yczeng@mit.edu>
2020-12-27 20:27:05 -0500

    sending keystates works

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,11 @@ extern crate websocket;
 use std::io::stdin;
 use std::sync::mpsc::channel;
 use std::thread;
+use std::net::TcpStream;
 
 use websocket::client::ClientBuilder;
+use websocket::sync::Client;
+use websocket::sender::Writer;
 use websocket::{Message, OwnedMessage};
 
 mod globals;
@@ -40,7 +43,7 @@ struct Player {
 
 struct Game {
 	p1: Player,
-	p2: Player
+	// p2: Player
 }
 
 fn main() {
@@ -76,12 +79,54 @@ fn main() {
 
   println!("Successfully connected");
 
+  let (mut receiver, mut sender) = client.split().unwrap();
+  let (tx, rx) = channel();
+  let tx_1 = tx.clone();
+
+  let receive_loop = thread::spawn(move || {
+    // Receive loop
+    for message in receiver.incoming_messages() {
+      let message = match message {
+        Ok(m) => m,
+        Err(e) => {
+          println!("Error receiving data from server: {:?}", e);
+          let _ = tx_1.send(OwnedMessage::Close(None));
+          return;
+        }
+      };
+
+      match message {
+        OwnedMessage::Close(_) => {
+          // Got a close message, so send a close message and return
+          let _ = tx_1.send(OwnedMessage::Close(None));
+          println!("Closing connection to server");
+          return;
+        }
+        OwnedMessage::Ping(data) => {
+          match tx_1.send(OwnedMessage::Pong(data)) {
+            // Send a pong in response
+            Ok(()) => (),
+            Err(e) => {
+              println!("Pinging server failed: {:?}", e);
+              return;
+            }
+          }
+        }
+        _ => { // update the local game state to the one we just received
+          // game = parse_game(&message);
+          println!("received: {:?}", message);
+        },
+      }
+    }
+  });
+
+  // Whenever we get a key input from the user, send it to the server
+  // 60 times a second, render the current game state stored in Game struct
   while let Some(event) = window.next() {
     match event {
-      Event::Input(input_args, _timestamp) => { send_key(&mut game, &input_args); },
+      Event::Input(input_args, _timestamp) => { send_key(&mut game, &input_args, &mut sender); },
       Event::Loop(loop_args) => {
         match loop_args {
-          // Loop::Update(update_args) => { update(&mut game, &update_args); },
           Loop::Render(render_args) => { render(&game, &mut window, event, &render_args); },
           _ => {}
         }},
@@ -90,13 +135,53 @@ fn main() {
   }
 }
 
-fn send_key(game: &mut Game, input: &Input) {
-  // send the keys to websocket
+fn send_key(game: &mut Game, input: &Input, sender: &mut Writer<TcpStream>) {
+
+  match input {
+    Input::Button(butargs) => {
+      match butargs.button {
+        Button::Keyboard(Key::Space) => {
+          let mut keystate = "";
+          if butargs.state == ButtonState::Press { 
+            keystate = "space-press";
+          } else if butargs.state == ButtonState::Release {
+            keystate = "space-release";
+          }
+          let message = OwnedMessage::Text(keystate.to_string());
+          sender.send_message(&message);
+          println!("tred to send: {}", keystate);
+        },
+        Button::Keyboard(Key::Left) => { 
+          let mut keystate = "";
+          if butargs.state == ButtonState::Press { 
+            keystate = "left-press";
+          } else if butargs.state == ButtonState::Release {
+            keystate = "left-release";
+          }
+          let message = OwnedMessage::Text(keystate.to_string());
+          sender.send_message(&message);
+          println!("tred to send: {}", keystate);
+        },
+        Button::Keyboard(Key::Right) => {
+          let mut keystate = "";
+          if butargs.state == ButtonState::Press { 
+            keystate = "right-press";
+          } else if butargs.state == ButtonState::Release {
+            keystate = "right-release";
+          }
+          let message = OwnedMessage::Text(keystate.to_string());
+          sender.send_message(&message);
+          println!("tred to send: {}", keystate);
+        },
+        _ => {}
+    }},
+    _ => {}
+  }
 }
 
 fn render(game: &Game, window: &mut PistonWindow, event: Event, render_args: &RenderArgs){
 	let p1 = &game.p1;
-	let p2 = &game.p2;
+	// let p2 = &game.p2;
 
 	window.draw_2d(&event, |c, g, _d| {
     clear([0.0, 0.0, 0.0, 1.0], g);	 // clear the screen
--- src/server.rs
+++ src/server.rs
@@ -53,7 +53,7 @@ fn main() {
 			println!("Connection from {}", ip);
 
 			// TODO: convert the game struct to a JSON and then send the JSON to the new client
-			let message = OwnedMessage::Text("Hello".to_string());
+			let message = OwnedMessage::Text("Flubbabubba".to_string());
 			client.send_message(&message).unwrap();
 
 			let (mut receiver, mut sender) = client.split().unwrap();
@@ -75,129 +75,131 @@ fn main() {
 						let message = OwnedMessage::Pong(ping);
 						sender.send_message(&message).unwrap();
 					}
-					_ => sender.send_message(&message).unwrap(),
+					_ => {
+						sender.send_message(&message).unwrap();
+					}
 				}
 			}
 		});
 	}
 }
 
-fn process_keys(game: &mut Game, input: &Input) {
-	match input {
-    Input::Button(butargs) => {
-    	match butargs.button {
-        Button::Keyboard(Key::Space) => { 
-        	if !game.p1.keys.jump && butargs.state == ButtonState::Press { 
-        		game.p1.keys.jump = true;
-        	}
-        	if game.p1.keys.jump && butargs.state == ButtonState::Release {
-        		game.p1.keys.jump = false;
-        	} 
-        },
-        Button::Keyboard(Key::Left) => { 
-        	if !game.p1.keys.left && butargs.state == ButtonState::Press { 
-        		game.p1.keys.left = true; 
-        	}
-        	if game.p1.keys.left && butargs.state == ButtonState::Release {
-        		game.p1.keys.left = false;
-        	}
-       	},
-        Button::Keyboard(Key::Right) => {
-        	if !game.p1.keys.right && butargs.state == ButtonState::Press { 
-        		game.p1.keys.right = true; 
-        	}
-        	if game.p1.keys.right && butargs.state == ButtonState::Release {
-        		game.p1.keys.right = false;
-        	}
-        },
-        _ => {}
-    }},
-    _ => {}
-  }
-}
+// fn process_keys(game: &mut Game, input: &Input) {
+// 	match input {
+//     Input::Button(butargs) => {
+//     	match butargs.button {
+//         Button::Keyboard(Key::Space) => { 
+//         	if !game.p1.keys.jump && butargs.state == ButtonState::Press { 
+//         		game.p1.keys.jump = true;
+//         	}
+//         	if game.p1.keys.jump && butargs.state == ButtonState::Release {
+//         		game.p1.keys.jump = false;
+//         	} 
+//         },
+//         Button::Keyboard(Key::Left) => { 
+//         	if !game.p1.keys.left && butargs.state == ButtonState::Press { 
+//         		game.p1.keys.left = true; 
+//         	}
+//         	if game.p1.keys.left && butargs.state == ButtonState::Release {
+//         		game.p1.keys.left = false;
+//         	}
+//        	},
+//         Button::Keyboard(Key::Right) => {
+//         	if !game.p1.keys.right && butargs.state == ButtonState::Press { 
+//         		game.p1.keys.right = true; 
+//         	}
+//         	if game.p1.keys.right && butargs.state == ButtonState::Release {
+//         		game.p1.keys.right = false;
+//         	}
+//         },
+//         _ => {}
+//     }},
+//     _ => {}
+//   }
+// }
 
-fn update(game: &mut Game, update_args: &UpdateArgs) {
-  // rewrite for each player in game, update player
-  update_player(&mut game.p1, update_args.dt);
-  // update_player(&mut game.p2, update_args.dt);
-}
+// fn update(game: &mut Game, update_args: &UpdateArgs) {
+//   // rewrite for each player in game, update player
+//   update_player(&mut game.p1, update_args.dt);
+//   // update_player(&mut game.p2, update_args.dt);
+// }
 
-fn update_player(p: &mut Player, dt: f64) {
-	// determine accelerations based on states
-	p.acc.y = -globals::GRAVITY;
-	if p.keys.jump {
-    if p.jump_time < globals::JUMP_DURATION {
-      p.acc.y = globals::JUMP_FORCE;
-      p.jump_time += 1;
-    }
-  } else if p.grounded {
-    p.jump_time = 0;
-  } else {
-    p.jump_time = globals::JUMP_DURATION;
-  }
+// fn update_player(p: &mut Player, dt: f64) {
+// 	// determine accelerations based on states
+// 	p.acc.y = -globals::GRAVITY;
+// 	if p.keys.jump {
+//     if p.jump_time < globals::JUMP_DURATION {
+//       p.acc.y = globals::JUMP_FORCE;
+//       p.jump_time += 1;
+//     }
+//   } else if p.grounded {
+//     p.jump_time = 0;
+//   } else {
+//     p.jump_time = globals::JUMP_DURATION;
+//   }
 
-	if p.keys.left { 
-    if p.drift_time < globals::DRIFT_DURATION {
-      p.acc.x = -globals::DRIFT_FORCE;
-      p.drift_time += 1;
-    }
-  } else if p.keys.right {
-    if p.drift_time < globals::DRIFT_DURATION {
-      p.acc.x = globals::DRIFT_FORCE;
-      p.drift_time += 1;
-    }
-  } else {
-    if p.vel.x >= globals::MIN_XSPEED {
-      if p.grounded {
-        p.acc.x -= globals::GND_DRAG_FORCE;
-      } else {
-        p.acc.x -= globals::AIR_DRAG_FORCE;
-      }
-      if p.vel.x + p.acc.x < globals::MIN_XSPEED {
-        p.vel.x = 0.0;
-        p.acc.x = 0.0;
-      }
-    }
-    if p.vel.x <= -globals::MIN_XSPEED {
-      if p.grounded {
-        p.acc.x += globals::GND_DRAG_FORCE;
-      } else {
-        p.acc.x += globals::AIR_DRAG_FORCE;
-      }
-      if p.vel.x + p.acc.x > -globals::MIN_XSPEED {
-        p.vel.x = 0.0;
-        p.acc.x = 0.0;
-      }
-    }
-    p.drift_time = 0;
-  }
+// 	if p.keys.left { 
+//     if p.drift_time < globals::DRIFT_DURATION {
+//       p.acc.x = -globals::DRIFT_FORCE;
+//       p.drift_time += 1;
+//     }
+//   } else if p.keys.right {
+//     if p.drift_time < globals::DRIFT_DURATION {
+//       p.acc.x = globals::DRIFT_FORCE;
+//       p.drift_time += 1;
+//     }
+//   } else {
+//     if p.vel.x >= globals::MIN_XSPEED {
+//       if p.grounded {
+//         p.acc.x -= globals::GND_DRAG_FORCE;
+//       } else {
+//         p.acc.x -= globals::AIR_DRAG_FORCE;
+//       }
+//       if p.vel.x + p.acc.x < globals::MIN_XSPEED {
+//         p.vel.x = 0.0;
+//         p.acc.x = 0.0;
+//       }
+//     }
+//     if p.vel.x <= -globals::MIN_XSPEED {
+//       if p.grounded {
+//         p.acc.x += globals::GND_DRAG_FORCE;
+//       } else {
+//         p.acc.x += globals::AIR_DRAG_FORCE;
+//       }
+//       if p.vel.x + p.acc.x > -globals::MIN_XSPEED {
+//         p.vel.x = 0.0;
+//         p.acc.x = 0.0;
+//       }
+//     }
+//     p.drift_time = 0;
+//   }
 
-	// integrate acceleration to get velocities
-	p.vel.x += p.acc.x * dt;
-	p.vel.y += p.acc.y * dt;
+// 	// integrate acceleration to get velocities
+// 	p.vel.x += p.acc.x * dt;
+// 	p.vel.y += p.acc.y * dt;
 
-	// integrate velocities to get positions
-	p.pos.x += p.vel.x * dt;
-	p.pos.y += p.vel.y * dt;
+// 	// integrate velocities to get positions
+// 	p.pos.x += p.vel.x * dt;
+// 	p.pos.y += p.vel.y * dt;
 
-  // do bounds checks (top and left)
-  if p.pos.y <= globals::BOX_HEIGHT {  // floor
-    p.pos.y = globals::BOX_HEIGHT + globals::MIN_YSPEED*dt;
-    p.vel.y = 0.0;
-  } else if p.pos.y >= globals::HEIGHT {  // ceiling
-    p.pos.y = globals::HEIGHT - globals::MIN_YSPEED*dt;
-    p.vel.y = 0.0;
-  }
-  if p.pos.x <= 0.0 {  // left
-    p.pos.x = 0.0 + globals::MIN_XSPEED*dt;
-    p.vel.x = 0.0;
-  } else if p.pos.x >= globals::WIDTH {  // right
-    p.pos.x = globals::WIDTH - globals::MIN_XSPEED*dt;
-    p.vel.x = 0.0;
-  }
+//   // do bounds checks (top and left)
+//   if p.pos.y <= globals::BOX_HEIGHT {  // floor
+//     p.pos.y = globals::BOX_HEIGHT + globals::MIN_YSPEED*dt;
+//     p.vel.y = 0.0;
+//   } else if p.pos.y >= globals::HEIGHT {  // ceiling
+//     p.pos.y = globals::HEIGHT - globals::MIN_YSPEED*dt;
+//     p.vel.y = 0.0;
+//   }
+//   if p.pos.x <= 0.0 {  // left
+//     p.pos.x = 0.0 + globals::MIN_XSPEED*dt;
+//     p.vel.x = 0.0;
+//   } else if p.pos.x >= globals::WIDTH {  // right
+//     p.pos.x = globals::WIDTH - globals::MIN_XSPEED*dt;
+//     p.vel.x = 0.0;
+//   }
 
-  p.grounded = p.pos.y <= globals::BOX_HEIGHT + globals::MIN_YSPEED*dt;
-  // println!("P[x:{}, y:{}, vx: {}, vy:{}, ax:{}, ay:{}]", p.pos.x, p.pos.y, p.vel.x, p.vel.y, p.acc.x, p.acc.y);
+//   p.grounded = p.pos.y <= globals::BOX_HEIGHT + globals::MIN_YSPEED*dt;
+//   // println!("P[x:{}, y:{}, vx: {}, vy:{}, ax:{}, ay:{}]", p.pos.x, p.pos.y, p.vel.x, p.vel.y, p.acc.x, p.acc.y);
 
-  // TODO: for all of these updates, send a message to server saying that the player's info has changed.
-}
+//   // TODO: for all of these updates, send a message to server saying that the player's info has changed.
+// }
